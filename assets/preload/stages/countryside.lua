bf9d9099ae93a5ba83fb461f14d1b2d4dfcdc947
MAT578590 <112621177+MAT578590@users.noreply.github.com>
2022-09-09 17:49:18 -0300

    Add files via upload

---
+++ assets/preload/stages/countryside.lua
@@ -0,0 +1,11 @@
+function onCreate()
+
+	makeLuaSprite('couragebg', 'couragebg', -396, -328);
+    addLuaSprite('couragebg', false);
+	
+makeAnimatedLuaSprite('pibby','pibby',1250,430)
+addAnimationByPrefix('pibby','bounce','pibby Idle',13,true)
+addLuaSprite('pibby',true)
+
+end
+
